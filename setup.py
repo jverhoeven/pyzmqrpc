27616ebe7c8d0ee8db85c6c6ba5594dd89f2b98c
Jan Verhoeven <jan@visity.nl>
2014-04-11 01:38:58 +0200

    Upping the version number for pypi

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name='pyzmqrpc',
     packages=['zmqrpc'],
-    version='0.1',
+    version='0.1.1',
     description='A simple ZMQ RPC extension on ZeroMQ',
     author='J Verhoeven',
     author_email='jan@visity.nl',
     url='https://github.com/jverhoeven/pyzmqrpc',
-    download_url='https://github.com/jverhoeven/pyzmqrpc/tarball/0.1',
+    download_url='https://github.com/jverhoeven/pyzmqrpc/tarball/0.1.1',
     keywords=['zeromq', 'rpc', 'pyzmq'],
     install_requires=["pyzmq"],
     classifiers=[]
